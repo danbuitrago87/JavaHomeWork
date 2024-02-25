d2e3dcdcbb3dd4793e6fa8173e5190f31e112f9c
unknown <danbuitrago87@gmail.com>
2024-02-25 10:05:22 -0500

    JavaHomeWork

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+    public static void main (String [] args) {
+        //CREACIÓN DE VARIABLES
+        String mother="Yolanda";
+        String father="Hermes";
+        String brother1="David";
+        String brother2="Jhon";
+        String brother3="Wilson";
+        byte brotherAge3=49;
+        String me="Daniel";
+        byte MyAge=37;
+        String Carrer="Java Programmer";
+        String like="Rock and Heavy Metal Music";
+        String band="Folk Music";
+        System.out.println("WELCOME");
+        System.out.println("This is my family.");
+        System.out.println("My dear mother is " + mother+".My father is " + father+".My big brother is " + brother2+".My second big brother is " + brother3 + " and he is " + brotherAge3 + " years old"+".My younger brother is " + brother1+
+                " And I am " + me+ ".I am " +MyAge+ " years old." + "I am a " +Carrer+ ".I love " +like+
+                " and I am in a Rock Band of " +band+ " and I like sancocho.");
+    }
+}
